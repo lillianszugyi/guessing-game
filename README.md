5f7d0f9f28dfb18f9f7f35b62f1977038c465846
Lillian Szugyi <lillian.szugyi@gmail.com>
2016-01-17 04:09:55 -0800

    big updates to guessing game script.js and index.html

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# guessing-game
-Class demo for guessing game.
+# Guessing Game
+This is a game that people can play in order to know me better.
+
+## Game Requirements
+1. Use prompts to ask questions and validate if the user is correct or incorrect.
+2. Use functions to reduce the amount of repeated lines of code.
+3. Ask user to guess number-related question and help them get to the right number by giving hints, e.g. "too high, too low"
+
+### Created by Lillian Szugyi
---
+++ js/script.js
@@ -0,0 +1,54 @@
+var userName = prompt("Hi there! What's your name?");
+  alert("Hey " + userName + ". Nice to meet you. Let's get started.");
+  console.log("variable userName: " + userName);
+
+var questions = [
+  {question: "Was I a cake decorator while I was in college?", answer: "yes"},
+  {question: "What is my favorite color?", answer: "black"},
+  {question: "Do I plan to move out of Seattle someday?", answer: "no"},
+  {question: "How many years did I play the flute?", answer: "15"},
+  {question: "I do CrossFit and I just hit a PR on my one rep max deadlift. Can you guess how much weight I lifted in pounds?", answer: "225"},
+  {question: "I love taking photos. What is my favorite subject?", answer: "dogs"}
+];
+
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var correct = [];
+var incorrect = [];
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function buildList(arr) {
+  var listHTML = '<ol>';
+    for (var i = 0; i < arr.length; i += 1) {
+      listHTML += '<li>' + arr[i] + '</li>';
+    }
+    listHTML += '</ol>';
+    return listHTML;
+}
+
+for (var i = 0; i < questions.length; i += 1) {
+  question = questions[i].question;
+  answer = questions[i].answer;
+  response = prompt(question).toLowerCase();
+  if (response === answer) {
+    correctAnswers += 1;
+    correct.push(question);
+    console.log("response: correct");
+  } else {
+    incorrect.push(question);
+    console.log("response: incorrect");
+  }
+}
+
+html = "You got " + correctAnswers + " question(s) right.";
+html += "<h3>You got these questions correct:</h3>";
+html += buildList(correct);
+html += "<h3>You got these questions incorrect:</h3>";
+html += buildList(incorrect);
+print(html);
